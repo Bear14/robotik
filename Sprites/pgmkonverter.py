b1818d69a10d3809b046dac18e5377a2f6368123
galaxyhiker <galaxyhiker42@web.de>
2019-03-05 17:37:01 +0100

    pgmkobverter erzeugt sprits.txt in der alle pages ab Startpunkt x,y angegen werden

--- Sprites/pgmkonverter.py
+++ Sprites/pgmkonverter.py
@@ -4,39 +4,78 @@
 import math
 
 if __name__ == '__main__':
-    x = 4
-    y = 64
-    sprite = [[2]*x for i in range(y)]
-    pages = [[2]*y for i in range(x)]
+    #WO soll das Bild beginnen Linkes Eck
+    x=0
+    y=0
+
+    picsize = 16  #wquatratisches Bild
+
+    pagesize = 4 #Fix
+
+    #pic = [[2]*picsize for i in range(picsize)]
+    picmirror = [[2]*picsize for i in range(picsize)]
+    pages = []
     k=0
+    h=0
+    buffer = "33"
     file = open("Hero.pgm", "r")
 
-    for line in file:
+    for line in file: #Werte werden 2bit weise angeordnet
         if k > 3:
-            print(k)
-            if line.rstrip() == "255":
-                sprite[math.floor((k-4)/y)][(k-4)%x] ="00"
-            elif line.rstrip() == "162":
-                sprite[math.floor((k-4)/x)][(k-4)%y]  ="01"
-            elif line.rstrip() == "114":
-                sprite[math.floor((k-4)/x)][(k-4)%y] ="10"
-            elif line.rstrip() == "62":
-                sprite[math.floor((k-4)/x)][(k-4)%y] ="11"
+            h = k-4
+
+            if line.rstrip() == "255":   buffer="00"
+            elif line.rstrip() == "162": buffer="01"
+            elif line.rstrip() == "114": buffer="10"
+            elif line.rstrip() == "62": buffer="11"
             else:
                 print("wert nicht erfasst")
                 break
+            # pic[Y-wert][X-Wert] Jeder Y-Wert bildet eine neue Liste in pic
+            #pic[math.floor(h/picsize)][h%picsize]=buffer
+
+            #Bild wird an der Hauptachse gespiegelt gespeichert
+            picmirror[h%picsize][math.floor(h/picsize)]=buffer
         k=k+1
 
     file.close()
 
-    print (sprite)
+    for i in range(len(picmirror)):
+        for j in range(0,len(picmirror[i]), 4):
+            #print(elem, end=' ')
+            print(i,"|",j,"  ")
+            pages.append(str(hex(int(picmirror[i][j]+picmirror[i][j+1]+
+            picmirror[i][j+2]+picmirror[i][j+3],2))) )
 
-    k=0
-    for row in sprite:
-        for elem in row:
-            print(elem, end=' ')
-        print()
+        print(pages)
 
     file2 = open("sprits.txt", "w")
+    for row in pages:
+        file2.write("page(")
+        file2.write(str(x))
+        file2.write(",")
+        file2.write(str(y))
+        file2.write(",")
+        for elem in row:
+            file2.write(elem)
+            file2.write("")
+        x=x+1
+        if x == 16:
+            x=0
+            y=y+1
+        file2.write(")\n")
 
+
+"""
     file2.close()
+
+    file3 = open("sprits2.txt", "w")
+
+    for i in range(picsize):
+        for j in range(picsize):
+            file3.write(pic[j][i])
+            file3.write(" ")
+        file3.write("\n")
+
+    file3.close()
+"""
