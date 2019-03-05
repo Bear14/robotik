cd3bcf7d28da9bf5a39b5712893b3c5a403272ce
Bear14 <bearlord14@gmail.com>
2019-03-05 15:09:27 +0100

    Slow down

--- Console/Master/main.c
+++ Console/Master/main.c
@@ -10,24 +10,26 @@
 #include "timer.h"
 #include "buttons.h"
 #include "display.h"
+#include <avr/interrupt.h>
 
 #define DASHLENGTH 6
-#define JUMPHEIGHT -5
+#define JUMPHEIGHT -8
 #define GRAVITY 1
 
 enum state{ falling,standing,dashing0,jumping,dashing1,doubleJumping,dashing2};
 
 
-uint8_t cloudX = 150;
-uint8_t cloudY = 52;
-uint8_t cloudLen = 20;
-uint8_t lastCloudX = 150;
-uint8_t lastCloudY = 52;
+int8_t cloudX = 150;
+int8_t cloudY = 52;
+int8_t cloudLen = 20;
+int8_t lastCloudX = 150;
+int8_t lastCloudY = 52;
 
 int8_t playerPos = 52; //change to 0 - 103
-int8_t lastPlayerPos = 56;
+int8_t lastPlayerPos = 52;
 //float gravity = 1;
-uint32_t speed = 1;
+int32_t speed = 1;
+
 
 uint8_t groundPos = 100;
 
@@ -36,6 +38,8 @@ enum state playerState;
 char inJump = '0';
 char inDoubleJump = '0';
 int8_t playerMov = 0;
+char b_pressed = '0';
+int32_t b_time = 0;
 
 int8_t dashing;
 
@@ -117,25 +121,6 @@ void updatePlayerPos() {
         playerState = falling;
     }
 
-/*
-    lastPlayerPos = playerPos;
-    if (dashing > 0) {
-
-        dashing--;
-
-
-    } else {
-
-
-        if (inJump == '1') {
-            playerMov -= gravity;
-            playerPos -= playerMov;
-        } else if (inJump == '0') {
-
-        }
-    }
-
-*/
 }
 
 
@@ -145,13 +130,13 @@ void updateCloud() {
 }
 
 void update() {
-    if (dashing > 0) {
-        speed *= 2;
-    } else {
-        speed = 1;
+
+    if(getMsTimer() % 14 == 0) {
+        updateCloud();
+        updatePlayerPos();
+
     }
-    updateCloud();
-    updatePlayerPos();
+
 
 }
 
@@ -174,27 +159,31 @@ void pageTest() {
 }
 
 
-void drawRect(uint8_t pos,uint8_t lastPos) {
+void drawRect(int8_t pos,int8_t lastPos) {
     if(pos == lastPos){
 
     }
     else {
 
+        //clear
+
+        uint8_t last = lastPos /4;
+        page(5,last-1,0);
+        page(6,last-1,0);
+        page(7,last-1,0);
+        page(8,last-1,0);
+        page(5,last,0);
+        page(6,last,0);
+        page(7,last,0);
+        page(8,last,0);
+        page(5,last+1,0);
+        page(6,last+1,0);
+        page(7,last+1,0);
+        page(8,last+1,0);
+
+
         if (pos % 4 == 0) {
             uint8_t nex = playerPos / 4;
-
-            page(5, nex - 2, 0);
-            page(6, nex - 2, 0);
-            page(7, nex - 2, 0);
-            page(8, nex - 2, 0);
-            page(5, nex - 1, 0);
-            page(6, nex - 1, 0);
-            page(7, nex - 1, 0);
-            page(8, nex - 1, 0);
-            page(5, nex + 1, 0);
-            page(6, nex + 1, 0);
-            page(7, nex + 1, 0);
-            page(8, nex + 1, 0);
 
    //         clearPlayerColumn();
 
@@ -207,21 +196,6 @@ void drawRect(uint8_t pos,uint8_t lastPos) {
 
         } else if (pos % 4 == 1) {
             uint8_t nex = playerPos / 4;
-
-            page(5, nex - 2, 0);
-            page(6, nex - 2, 0);
-            page(7, nex - 2, 0);
-            page(8, nex - 2, 0);
-            page(5, nex, 0);
-            page(6, nex, 0);
-            page(7, nex, 0);
-            page(8, nex, 0);
-            page(5, nex + 1, 0);
-            page(6, nex + 1, 0);
-            page(7, nex + 1, 0);
-            page(8, nex + 1, 0);
-
-
    //         clearPlayerColumn();
             //draw
             page(5, nex, 0xFC);
@@ -235,20 +209,6 @@ void drawRect(uint8_t pos,uint8_t lastPos) {
 
         } else if (pos % 4 == 2) {
             uint8_t nex = playerPos / 4;
-
-
-            page(5, nex - 2, 0);
-            page(6, nex - 2, 0);
-            page(7, nex - 2, 0);
-            page(8, nex - 2, 0);
-            page(5, nex, 0);
-            page(6, nex, 0);
-            page(7, nex, 0);
-            page(8, nex, 0);
-            page(5, nex + 1, 0);
-            page(6, nex + 1, 0);
-            page(7, nex + 1, 0);
-            page(8, nex + 1, 0);
 
    //         clearPlayerColumn();
             //draw
@@ -265,21 +225,6 @@ void drawRect(uint8_t pos,uint8_t lastPos) {
         } else if (pos % 4 == 3) {
             uint8_t nex = playerPos / 4;
 
-
-            page(5, nex - 2, 0);
-            page(6, nex - 2, 0);
-            page(7, nex - 2, 0);
-            page(8, nex - 2, 0);
-            page(5, nex, 0);
-            page(6, nex, 0);
-            page(7, nex, 0);
-            page(8, nex, 0);
-            page(5, nex + 1, 0);
-            page(6, nex + 1, 0);
-            page(7, nex + 1, 0);
-            page(8, nex + 1, 0);
-
-            //clearPlayerColumn();
 
             //draw
             page(5, nex, 0xC0);
@@ -299,55 +244,77 @@ void drawRect(uint8_t pos,uint8_t lastPos) {
 
 void draw() {
     drawRect(playerPos,lastPlayerPos);
-    //drawCloud();
+    drawCloud();
 }
 
 
 
 void getInput() {
 
-    if (B_SELECT) {
-        //uart_putc(20);
-    }
-    if (B_PAUSE) {
-        //uart_putc(30);
+    if(b_time + 40 == getMsTimer()){
+        b_pressed = '0';
     }
 
-    if (B_UP) {
-        _delay_ms(40);
-        lastPlayerPos--;
-        playerPos--;
-        //uart_putc(50);
-        //updatePlayerPos(playerPos -1 );
-    }
-    if (B_DOWN) {
-        _delay_ms(40);
-        lastPlayerPos++;
-        playerPos++;
-        //updatePlayerPos(playerPos + 1);
-        //uart_putc(60);
-    }
-    if (B_RIGHT) {
-        _delay_ms(40);
-        playerState = standing;
-        //uart_putc(70);
-    }
-    if (B_LEFT) {
+        if (B_SELECT) {
+            //uart_putc(20);
+        }
+        if (B_PAUSE) {
+            //uart_putc(30);
+        }
 
-        //uart_putc(80);
-    }
-    if (B_A) {
-        _delay_ms(40);
-        dash();
-        uart_putc(90);
-    }
-    //
-    if (B_B) {
-        _delay_ms(40);
-        jump();
-        uart_putc(100);
-    }
-    //uart_putc(100);
+        if (B_UP) {
+            if (b_pressed == '0') {
+                b_pressed = '1';
+                b_time = getMsTimer();
+            }
+            lastPlayerPos--;
+            playerPos--;
+            //uart_putc(50);
+            //updatePlayerPos(playerPos -1 );
+        }
+        if (B_DOWN) {
+            if (b_pressed == '0') {
+                b_pressed = '1';
+                b_time = getMsTimer();
+            }
+            lastPlayerPos++;
+            playerPos++;
+            //updatePlayerPos(playerPos + 1);
+            //uart_putc(60);
+        }
+        if (B_RIGHT) {
+            if (b_pressed == '0') {
+                b_pressed = '1';
+                b_time = getMsTimer();
+            }
+            playerState = standing;
+            //uart_putc(70);
+        }
+        if (B_LEFT) {
+            if (b_pressed == '0') {
+                b_pressed = '1';
+                b_time = getMsTimer();
+            }
+            //uart_putc(80);
+        }
+        if (B_A) {
+            if (b_pressed == '0') {
+                b_pressed = '1';
+                b_time = getMsTimer();
+            }
+            dash();
+            uart_putc(90);
+        }
+        //
+        if (B_B) {
+            if (b_pressed == '0') {
+                b_pressed = '1';
+                b_time = getMsTimer();
+            }
+            jump();
+            uart_putc(100);
+        }
+        //uart_putc(100);
 
 
 }
@@ -364,7 +331,6 @@ int main(void) {
         collision();
         update();
         draw();
-
 
 
     }
