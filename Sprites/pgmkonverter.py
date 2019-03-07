77b10933eff4376516f652e3ba3ff7da02c23664
galaxyhiker <galaxyhiker42@web.de>
2019-03-07 19:15:14 +0100

    Buchstaben Zahle und änderungen an pgmkonverter

--- Sprites/pgmkonverter.py
+++ Sprites/pgmkonverter.py
@@ -1,43 +1,44 @@
-""" .pgm Bilddatei in ein aray umwandeln
 
-"""
+#.pgm Bilddatei in ein aray umwandeln
 import math
 
 if __name__ == '__main__':
     #WO soll das Bild beginnen Linkes Eck
 
-    picsize = 16  #wquatratisches Bild
+    quell = input("Quelldatei: (nur .pgm)")
+    ziel = input("Name der Zieldatei: ")
+
+    quellfile = open(quell, "r")
+    zielfile = open(ziel, "w")
+
+    quelllines = quellfile.read().splitlines()
+    picwidth = int(quelllines[2].split(" ")[0])
+    picheight = int(quelllines[2].split(" ")[1])
+
+    print(picheight)
+    print(picwidth)
 
     x=0
     y=0
-    pic = [[2]*picsize for i in range(picsize)]
     pages = []
-    k=0 #zaehlt die Zeilen
-    h=0 #zaehlt ab dem ersten Pixel'
-    buffer = "33"
+    pic = [[2]*picheight for i in range(picwidth)]
 
-    quell = input("Quelldatei: (nur .pgm)")
-    ziel = input("Name der Zieldatei: ")
-    quellfile = open(quell, "r")
-    zielfile = open(ziel, "w")
-    for line in quellfile: #Werte werden 2bit weise angeordnet
+    buffer = '33'
+    quelllines=quelllines[4:]
+    quellfile.close()
+    for i in range(len(quelllines)): #Werte werden 2bit weise angeordnet
         #Die ersten 4 Zeilen dienern der Beschreibung der Datei
         #Sind fuer uns irrelevant
-        if k > 3:
-            if line.rstrip() == "255": buffer="00"
-            elif line.rstrip() == "162": buffer="01"
-            elif line.rstrip() == "114": buffer="10"
-            elif line.rstrip() == "62": buffer="11"
-            else:
-                print("wert nicht erfasst")
-                break
+        if quelllines[i]== "255": buffer="00"
+        elif quelllines[i] == "162": buffer="01"
+        elif quelllines[i]== "114": buffer="10"
+        elif quelllines[i] == "62": buffer="11"
+        else:
+            print("wert nicht erfasst")
+            break
+        #Bild wird an der Hauptachse gespiegelt gespeichert
+        pic[i%picwidth][int(math.floor(i/picwidth))] =str(buffer)
 
-            #Bild wird an der Hauptachse gespiegelt gespeichert
-            pic[h%picsize][int(math.floor(h/picsize))] =str(buffer)
-
-            h=h+1
-        k=k+1
-    quellfile.close()
 
     for i in range(len(pic)):
         for j in range(0,len(pic[i]), 4):
@@ -49,7 +50,7 @@ if __name__ == '__main__':
 
         y=y+1
         #Am ende der ersten Pixelzeile in die naechste Pixelzeile springen
-        if y == int(picsize/4):
+        if y == int(picheight/4):
             y=0
             x=x+1
         zielfile.write(");\n")
