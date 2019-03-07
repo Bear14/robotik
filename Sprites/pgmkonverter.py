b7dc16a9e33fe77142ac26dc8f08b5979e0362bc
galaxyhiker <galaxyhiker42@web.de>
2019-03-07 15:06:22 +0100

    Annahme: Bild 16x16 Pixel, Eingabe: Quelldatei und Zieldatei

--- Sprites/pgmkonverter.py
+++ Sprites/pgmkonverter.py
@@ -1,110 +1,57 @@
 """ .pgm Bilddatei in ein aray umwandeln
 
 """
-<<<<<<< HEAD
-if __name__ == '__main__':
-    sprite = []
-    k=0
-    file = open("Hero.pgm", "r")
-
-    for line in file:
-        if k > 3:
-            if line.rstrip() == "255":
-                sprite.append("00")
-            elif line.rstrip() == "162":
-                sprite.append("01")
-            elif line.rstrip() == "114":
-                sprite.append("10")
-            elif line.rstrip() == "62":
-                sprite.append("11")
-        k=k+1
-    k=0
-    print(sprite)
-    file.close()
-    file2 = open("sprits.txt", "w")
-    for i in range (16):
-        for j in range (16):
-            file2.write(sprite[k])
-            k=k+1
-        file2.write("\n")
-    file2.close()
-=======
 import math
 
 if __name__ == '__main__':
     #WO soll das Bild beginnen Linkes Eck
-    x=0
-    y=0
 
     picsize = 16  #wquatratisches Bild
 
-    pagesize = 4 #Fix
-
-    #pic = [[2]*picsize for i in range(picsize)]
-    picmirror = [[2]*picsize for i in range(picsize)]
+    x=0
+    y=0
+    pic = [[2]*picsize for i in range(picsize)]
     pages = []
-    k=0
-    h=0
+    k=0 #zaehlt die Zeilen
+    h=0 #zaehlt ab dem ersten Pixel'
     buffer = "33"
-    file = open("Hero.pgm", "r")
 
-    for line in file: #Werte werden 2bit weise angeordnet
+    quell = input("Quelldatei: (nur .pgm)")
+    ziel = input("Name der Zieldatei: ")
+    quellfile = open(quell, "r")
+    zielfile = open(ziel, "w")
+    for line in quellfile: #Werte werden 2bit weise angeordnet
+        #Die ersten 4 Zeilen dienern der Beschreibung der Datei
+        #Sind fuer uns irrelevant
         if k > 3:
-            h = k-4
-
-            if line.rstrip() == "255":   buffer="00"
+            if line.rstrip() == "255": buffer="00"
             elif line.rstrip() == "162": buffer="01"
             elif line.rstrip() == "114": buffer="10"
             elif line.rstrip() == "62": buffer="11"
             else:
                 print("wert nicht erfasst")
                 break
-            # pic[Y-wert][X-Wert] Jeder Y-Wert bildet eine neue Liste in pic
-            #pic[math.floor(h/picsize)][h%picsize]=buffer
 
             #Bild wird an der Hauptachse gespiegelt gespeichert
-            picmirror[h%picsize][math.floor(h/picsize)]=buffer
+            pic[h%picsize][int(math.floor(h/picsize))] =str(buffer)
+
+            h=h+1
         k=k+1
+    quellfile.close()
 
-    file.close()
+    for i in range(len(pic)):
+        for j in range(0,len(pic[i]), 4):
+            #Das das Display von unten nach oben die Pages Schreibt
+            pages.append(int(pic[i][j+3] + pic[i][j+2] + pic[i][j+1] + pic[i][j] ,2))
 
-    for i in range(len(picmirror)):
-        for j in range(0,len(picmirror[i]), 4):
-            #print(elem, end=' ')
-            print(i,"|",j,"  ")
-            pages.append(str(hex(int(picmirror[i][j]+picmirror[i][j+1]+
-            picmirror[i][j+2]+picmirror[i][j+3],2))) )
+    for elem in pages:
+        zielfile.write("page(x+"+str(x)+",y+"+str(y)+",0x%02X" %(elem))
 
-        print(pages)
+        y=y+1
+        #Am ende der ersten Pixelzeile in die naechste Pixelzeile springen
+        if y == int(picsize/4):
+            y=0
+            x=x+1
+        zielfile.write(");\n")
 
-    file2 = open("sprits.txt", "w")
-    for row in pages:
-        file2.write("page(")
-        file2.write(str(x))
-        file2.write(",")
-        file2.write(str(y))
-        file2.write(",")
-        for elem in row:
-            file2.write(elem)
-            file2.write("")
-        x=x+1
-        if x == 16:
-            x=0
-            y=y+1
-        file2.write(")\n")
-
-
-"""
-    file2.close()
-
-    file3 = open("sprits2.txt", "w")
-
-    for i in range(picsize):
-        for j in range(picsize):
-            file3.write(pic[j][i])
-            file3.write(" ")
-        file3.write("\n")
-
-    file3.close()
-"""
->>>>>>> 12b3af66ba108449f5f23f186c6ae3115058bbfc
+    zielfile.close()
