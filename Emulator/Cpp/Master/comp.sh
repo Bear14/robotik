e065cbf5ac4522cf42d8d022486a1a159bd3e71d
galaxyhiker <galaxyhiker42@web.de>
2019-03-11 20:33:35 +0100

    für mich Boost abgepasst

--- Emulator/Cpp/Master/comp.sh
+++ Emulator/Cpp/Master/comp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 
-g++ -I /home/torben/Install/boost/boost_1_66_0 $1 -o $2 /home/torben/Install/boost/lib/libboost_system.a /home/torben/Install/boost/lib/libboost_thread.a -pthread
+g++ -I /home/fabian/Install/boost/boost_1_66_0 $1 -o $2 /home/fabian/Install/boost/lib/libboost_system.a /home/fabian/Install/boost/lib/libboost_thread.a -pthread
 
