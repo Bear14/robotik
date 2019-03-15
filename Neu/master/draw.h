9365f6f34f875eb70f87e43ebc357e0645b801d0
Bear14 <bearlord14@gmail.com>
2019-03-15 14:43:40 +0100

    clean

--- Neu/master/draw.h
+++ Neu/master/draw.h
@@ -25,7 +25,5 @@ void drawCorrect(int16_t x, int16_t y, uint8_t h);
 void drawString(char text[],uint8_t x,uint8_t y);
 uint8_t nextGlyph(char glyph, uint8_t x, uint8_t y);
 void drawGlyph(char glyph, uint8_t x, uint8_t y);
-//void combineCollidingPages();
-void menue();
 void drawPfeil(uint8_t x, uint8_t y);
 #endif //ROBOTIK_DRAW_H
--- Neu/master/sprites.c
+++ Neu/master/sprites.c
@@ -5,73 +5,45 @@
 #include "sprites.h"
 #include "draw.h"
 
-void printPlayer(uint8_t x, uint8_t y){
+void printPlayer(int16_t x, int16_t y, int16_t lastY) {
+
+    if (y != lastY) {
+
+        drawCorrect(x + 0, lastY + 4, 0);
+        drawCorrect(x + 0, lastY + 0, 0);
+        drawCorrect(x + 1, lastY + 4, 0);
+        drawCorrect(x + 1, lastY + 0, 0);
+        drawCorrect(x + 2, lastY + 4, 0);
+        drawCorrect(x + 2, lastY + 0, 0);
+        drawCorrect(x + 3, lastY + 4, 0);
+        drawCorrect(x + 3, lastY + 0, 0);
+        drawCorrect(x + 4, lastY + 4, 0);
+        drawCorrect(x + 4, lastY + 0, 0);
+        drawCorrect(x + 5, lastY + 4, 0);
+        drawCorrect(x + 5, lastY + 0, 0);
+        drawCorrect(x + 6, lastY + 4, 0);
+        drawCorrect(x + 6, lastY + 0, 0);
+        drawCorrect(x + 7, lastY + 0, 0);
+        drawCorrect(x + 7, lastY + 4, 0);
 
 
-    drawCorrect(x+0,y+0,0x00);
-    drawCorrect(x+0,y+4,0x00);
-    drawCorrect(x+0,y+8,0x00);
-    drawCorrect(x+0,y+12,0x00);
-    drawCorrect(x+1,y+0,0x00);
-    drawCorrect(x+1,y+4,0x00);
-    drawCorrect(x+1,y+8,0x00);
-    drawCorrect(x+1,y+12,0x00);
-    drawCorrect(x+2,y+0,0x00);
-    drawCorrect(x+2,y+4,0x00);
-    drawCorrect(x+2,y+8,0x00);
-    drawCorrect(x+2,y+12,0x00);
-    drawCorrect(x+3,y+0,0x00);
-    drawCorrect(x+3,y+4,0x00);
-    drawCorrect(x+3,y+8,0x00);
-    drawCorrect(x+3,y+12,0x00);
-    drawCorrect(x+4,y+0,0x00);
-    drawCorrect(x+4,y+4,0x00);
-    drawCorrect(x+4,y+8,0x10);
-    drawCorrect(x+4,y+12,0xFC);
-    drawCorrect(x+5,y+0,0x00);
-    drawCorrect(x+5,y+4,0x00);
-    drawCorrect(x+5,y+8,0x0C);
-    drawCorrect(x+5,y+12,0x28);
-    drawCorrect(x+6,y+0,0xF0);
-    drawCorrect(x+6,y+4,0x5F);
-    drawCorrect(x+6,y+8,0x03);
-    drawCorrect(x+6,y+12,0x0A);
-    drawCorrect(x+7,y+0,0x7C);
-    drawCorrect(x+7,y+4,0x55);
-    drawCorrect(x+7,y+8,0x03);
-    drawCorrect(x+7,y+12,0x0A);
-    drawCorrect(x+8,y+0,0x7C);
-    drawCorrect(x+8,y+4,0x55);
-    drawCorrect(x+8,y+8,0xBF);
-    drawCorrect(x+8,y+12,0x02);
-    drawCorrect(x+9,y+0,0x7F);
-    drawCorrect(x+9,y+4,0x55);
-    drawCorrect(x+9,y+8,0xBF);
-    drawCorrect(x+9,y+12,0x02);
-    drawCorrect(x+10,y+0,0x7F);
-    drawCorrect(x+10,y+4,0x55);
-    drawCorrect(x+10,y+8,0xBF);
-    drawCorrect(x+10,y+12,0x02);
-    drawCorrect(x+11,y+0,0x7F);
-    drawCorrect(x+11,y+4,0x77);
-    drawCorrect(x+11,y+8,0xBF);
-    drawCorrect(x+11,y+12,0x02);
-    drawCorrect(x+12,y+0,0x7F);
-    drawCorrect(x+12,y+4,0x75);
-    drawCorrect(x+12,y+8,0x83);
-    drawCorrect(x+12,y+12,0xEA);
-    drawCorrect(x+13,y+0,0x3F);
-    drawCorrect(x+13,y+4,0x00);
-    drawCorrect(x+13,y+8,0x03);
-    drawCorrect(x+13,y+12,0xEA);
-    drawCorrect(x+14,y+0,0x0F);
-    drawCorrect(x+14,y+4,0xC0);
-    drawCorrect(x+14,y+8,0x00);
-    drawCorrect(x+14,y+12,0xC0);
-    drawCorrect(x+15,y+0,0x0F);
-    drawCorrect(x+15,y+4,0x10);
-    drawCorrect(x+15,y+8,0x00);
-    drawCorrect(x+15,y+12,0x00);
+        drawCorrect(x + 0, y + 4, 0xF8);
+        drawCorrect(x + 1, y + 0, 0x02);
+        drawCorrect(x + 1, y + 4, 0x02);
+        drawCorrect(x + 2, y + 0, 0xAA);
+        drawCorrect(x + 2, y + 4, 0xAE);
+        drawCorrect(x + 3, y + 0, 0x7A);
+        drawCorrect(x + 3, y + 4, 0x2E);
+        drawCorrect(x + 4, y + 0, 0x5A);
+        drawCorrect(x + 4, y + 4, 0x2E);
+        drawCorrect(x + 5, y + 0, 0x78);
+        drawCorrect(x + 5, y + 4, 0xAE);
+        drawCorrect(x + 6, y + 0, 0x03);
+        drawCorrect(x + 6, y + 4, 0xC2);
+        drawCorrect(x + 7, y + 0, 0xFC);
+        drawCorrect(x + 7, y + 4, 0x3D);
 
 
+    }
 }
+
