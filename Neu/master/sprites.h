c7a292ff440f37d1f1133111f97dbd9ae7fccc71
galaxyhiker <galaxyhiker42@web.de>
2019-03-25 14:42:33 +0100

    Add printScreem, printTitel change print Dragon

--- Neu/master/sprites.h
+++ Neu/master/sprites.h
@@ -24,6 +24,10 @@ void printPowerUp(int16_t x, int16_t y, enum PowerUpType type, int16_t speed);
 
 void clearPowerUp(int16_t x, int16_t y);
 
-void printDragon();
+void printDragon(uint8_t x, uint8_t y);
+
+void printTitel(uint8_t x, uint8_t y);
+
+void printScreen();
 
 #endif //ROBOTIK_SPRITES_H
