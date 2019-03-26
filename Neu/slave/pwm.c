554a9111dbcfa45d4cb68c1d8aaa7c9195ebe089
galaxyhiker <galaxyhiker42@web.de>
2019-03-26 18:37:34 +0100

    Ton

--- Neu/slave/pwm.c
+++ Neu/slave/pwm.c
@@ -8,14 +8,20 @@
 //Pulsweite initialisieren
 void PWMInit()
 {
+    DDRB |= (1 << 1); //Pin 1 an PORTB auf Ausgang stellen
+
+    TCCR1A = (1 << WGM12) | (1 << COM1A0)| (1 << COM1A1); //CTC Mode wechslenm wenn OCR1A erreicht
+    TCCR1B = (1<<CS12) | (1<<CS10); //PRESCALE 1024
+    /*
 	DDRD |=  (1 << 6); //Pin 6 an PORTD auf Ausgang stellen
 	TCCR0A = (1<<WGM00)|(1<<COM0A1); //  Timer/Counter als nicht invertierenden 8-Bit PWM
 	TCCR0B = (1<<CS01) | (1<<CS00); //  Takt von CK / 64 generieren
+     */
 }
 
 //Pulsweite setzen
 void setPWM(uint8_t pwmWert)
 {
 	//Wert in das Register schreiben
-	OCR0A = pwmWert;
+	OCR1A = pwmWert;
 }
