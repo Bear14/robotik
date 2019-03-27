c5d76ce156f9271819c79b7011a17d4967f425ea
galaxyhiker <galaxyhiker42@web.de>
2019-03-27 18:35:32 +0100

    Neue Sounds

--- Neu/slave/main.c
+++ Neu/slave/main.c
@@ -15,7 +15,7 @@
 
 struct note {
     uint16_t frequenz;
-    uint16_t lenght ;
+    uint16_t lenght;
 };
 
 const uint8_t winSize = 6;
@@ -45,9 +45,48 @@ const struct note allNotes[] = {{C2S},
                                 {C6S},
                                 {E6S}};
 
-const uint8_t testSize = 10;
-const struct note test[] ={{C4,10}, {E3, 10},{C4,10}, {E3, 10},{C4,10}, {E3, 10},{C4,10}, {E3, 10},{C4,10}, {E3, 10}};
+const uint8_t colleSize = 10;
+const struct note colle[10] = {{C4, 50},
+                               {E3, 50},
+                               {C4, 50},
+                               {E3, 50},
+                               {C4, 50},
+                               {E3, 50},
+                               {C4, 50},
+                               {E3, 50},
+                               {C4, 50},
+                               {E3, 50}};
 
+const uint8_t powerUpSize = 10;
+const struct note powerUp[10] = {{E4, 40},
+                                 {C5, 40},
+                                 {F4, 40},
+                                 {D5, 40},
+                                 {A4, 40},
+                                 {E5, 40},
+                                 {B4, 40},
+                                 {F5, 40},
+                                 {C5, 40},
+                                 {A5, 40}
+
+};
+const uint8_t powerDownSize = 10;
+const struct note powerDown[10] = {{A5, 40},
+                                   {C5, 40},
+                                   {F5, 40},
+                                   {B4, 40},
+                                   {E5, 40},
+                                   {A4, 40},
+                                   {D5, 40},
+                                   {F4, 40},
+                                   {C5, 40},
+                                   {E4, 40}
+
+};
+const uint8_t gameOverSize = 7;
+const struct note gameOver[7]={{G4,250},{D4,250},{E4,25}, {D4,25},{E4,25}, {D4,25},{E4,25}, {D4,25},{E4,25}, {D4,25},{E4,25}, {E3,25}
+
+};
 void init();
 
 void playNote(struct note ton) {
@@ -57,7 +96,7 @@ void playNote(struct note ton) {
     while (scale) {
 
         for (uint8_t i = 0; i < scale; i++) {
-            _delay_us(10);
+            _delay_ms(1);
         }
 
         PORTB ^= (1 << 1);
@@ -81,12 +120,12 @@ void playMusik(const struct note musik[]) {
     }
 }
 
-void playPWM(const struct note musik[],uint8_t length) {
+void playPWM(const struct note musik[], uint8_t length) {
     uint16_t pwm = 0;
     DDRB |= (1 << 1);// Sound-Ausgang einschalten
 
     for (uint32_t i = 0; i < length; ++i) {
-        pwm = 62500 / (musik[i].frequenz * 2);
+        pwm = 65000 / (musik[i].frequenz * 2);
         setPWM(pwm);
         for (int16_t j = 0; j < musik[i].lenght; ++j) {
             _delay_ms(1);
@@ -96,12 +135,14 @@ void playPWM(const struct note musik[],uint8_t length) {
 
 }
 
+
+
 int main(void) {
     //Initialisierung ausfuehren
 
     init();
 
-   // DDRB |= (1 << 1); //in Init PWM()
+    // DDRB |= (1 << 1); //in Init PWM()
     //PORTB |= (1 << 1);
 
     uint8_t d = 0;
@@ -109,19 +150,31 @@ int main(void) {
     //uint16_t counter = 0;
     while (1) {
 
-        playPWM(test, testSize);
-
+        playPWM(gameOver, gameOverSize);
         //playPWM(allNotes, allNotesSize);
 
-        //_delay_ms(1000);
+        _delay_ms(1000);
 
         switch (d) {
             case '1' :
-                playPWM(test, testSize);
+                //playPWM(test, testSize);
                 break;
             case '2':
                 break;
             case '3':
+                break;
+            case '4':
+                playPWM(colle, colleSize);
+                break;
+            case '5':
+                playPWM(powerUp, powerUpSize);
+                break;
+            case '6':
+                playPWM(powerDown, powerDownSize);
+                break;
+            case '7':
+                break;
+            case '8':
                 break;
             default:
                 break;
@@ -133,7 +186,7 @@ int main(void) {
             d = uart_getc();
     }
 
-    uint16_t counter=0;
+    uint16_t counter = 0;
 
     while (1) {
 
