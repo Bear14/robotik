74624761f23ebb2996fd202cfe7de23437cb14f5
galaxyhiker <galaxyhiker42@web.de>
2019-03-27 17:02:26 +0100

    Englische notation und frequenzen

---
+++ Neu/slave/mynote.h
@@ -0,0 +1,139 @@
+#define tempo 10000 // in Milisekunden
+
+//pause
+#define PAUSES 100, 10000 / tempo
+#define PAUSEA 100, 10000 / tempo * 2
+#define PAUSEV 100, 10000 / tempo * 4
+#define PAUSEH 100, 10000 / tempo * 8
+#define PAUSEG 100, 10000 / tempo * 16
+
+//noten frequenz / 1000000
+#define C2  65.7
+#define D2  73.75
+#define E2  82.78
+#define F2  87.7
+#define G2  98.44
+#define A2  110.5
+#define B2  124.03
+#define C3  131.41
+#define D3  147.5
+#define E3  165.56
+#define F3  175.41
+#define G3  196.89
+#define A3  221
+#define B3  248.06
+#define C4  262.81
+#define D4  295
+#define E4  331.13
+#define F4  350.82
+#define G4  393.78
+#define A4  442
+#define B4  496.13
+#define C5  525.63
+#define D5  590
+#define E5  662.25
+#define F5  701.63
+#define G5  787.55
+#define A5  884
+#define B5  992.26
+#define C6  1051.26
+#define D6  1180
+#define E6  1324.5
+#define F6  1403.26
+#define G6  1575.11
+#define A6  1768
+#define B6  1984.51
+#define C7  2102.52
+
+//16 noten
+#define C2S  C2, tempo / 16
+
+#define C3S  C3, tempo / 16
+#define G3S  G3, tempo / 16
+
+#define C4S  C4, tempo / 16
+#define D4S  D4, tempo / 16
+#define E4S  E4, tempo / 16
+#define F4S  F4, tempo / 16
+#define FIS4S  FIS4, tempo / 16
+#define G4S  G4, tempo / 16
+#define GIS4S  GIS4, tempo / 16
+#define A4S  A4, tempo / 16
+#define B4S  B4, tempo / 16
+
+
+#define C5S  C5, tempo / 16
+#define D5S  D5,  tempo / 16
+#define DIS5S  DIS5, tempo / 16
+#define DES5S  DES5, tempo / 16
+#define E5S  E5, tempo / 16
+#define F5S  F5,  tempo / 16
+#define G5S  G5, tempo / 16
+#define A5S  A5, tempo / 16
+#define B5S  B5, tempo / 16
+
+#define DIS6S  DIS6, tempo / 16
+#define E6S  E6, tempo / 16
+#define C6S  C6, tempo / 16
+
+
+//achtel noten
+#define C2A  C2, tempo / 8
+
+#define C3A  C3, tempo / 8
+#define G3A  G3, tempo / 8
+
+#define C4A  C4, tempo / 8
+#define D4A  D4, tempo / 8
+#define E4A  E4, tempo / 8
+#define F4A  F4, tempo / 8
+#define FIS4A  FIS4, tempo / 8
+#define G4A  G4, tempo / 8
+#define GIS4A  GIS4, tempo / 8
+#define A4A  A4, tempo / 8
+#define H4A  H4, tempo / 8
+
+#define C5A  C5, tempo / 8
+#define D5A  D5, tempo / 8
+#define DIS5A  DIS5, tempo / 8
+#define DES5A  DES5, tempo / 8
+#define E5A  E5, tempo / 8
+#define F5A  F5,  tempo / 8
+#define G5A  G5, tempo / 8
+
+#define DIS6A  DIS6, tempo / 8
+#define E6A  E6, tempo / 8
+
+//viertel note
+#define C2V  C2, tempo / 4
+
+#define C3V  C3, tempo / 4
+
+#define C4V  C4, tempo / 4
+#define E4V  E4, tempo / 4
+#define F4V  F4, tempo / 4
+#define G4V  G4, tempo / 4
+#define A4V  A4, tempo / 4
+#define H4V  H4, tempo / 4
+
+#define C5V  C5, tempo / 4
+#define D5V  D5, tempo / 4
+#define E5V  E5, tempo / 4
+#define G5V  G5, tempo / 4
+
+
+#define C6V  C6, tempo / 4
+#define E6V  E6, tempo / 4
+//halbe noten
+#define C2H  C2, tempo / 2
+
+#define G4H  G4, tempo / 2
+#define A4H  A4, tempo / 2
+
+//ganze note
+#define F4G  F4, tempo
+#define G4G  G4, tempo
+#define A4G  A4, tempo
+
+
+
